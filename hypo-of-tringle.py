ff8dc6a6e7843f1bbe3df3e0e470969d0ee1ad11
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-03-21 01:32:32 -0700

    Add files via upload

---
+++ hypo-of-tringle.py
@@ -0,0 +1,5 @@
+# hypotenus of a right angle trangle
+h=int(input("enter the height of a tringle: "))
+b=int(input("enter the base of a tringle: "))
+hypo=(h**2+b**2)**.5
+print("hypotenus of a tringle  is %d"%hypo)
