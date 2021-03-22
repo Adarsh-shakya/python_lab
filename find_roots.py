ef66901b2209db491e8396cb4ead0874ef9e8706
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-03-22 08:28:31 -0700

    find the roots of given quadratic equation

---
+++ find_roots.py
@@ -0,0 +1,8 @@
+# find the  roots Quadratic Equation
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+c=int(input("Enter the value of c: "))
+D=(b**2-4*a*c)**0.5
+r1=(-b-D)/(2*a)
+r2=(-b+D)/(2*a)
+print'roots are',r1,r2
