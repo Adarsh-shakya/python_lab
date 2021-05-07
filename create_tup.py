2fb8793db01d1dc39f6abb186cf8ecc23f5e2f81
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-05-07 22:14:29 +0530

    create a tuple

---
+++ create_tup.py
@@ -0,0 +1,6 @@
+#create a tuple  
+a=()
+for i in range(1,10):
+    n=int(input('Enter the number: '))
+    a += (n,)
+print(a)    
---
+++ create_tup2.py
@@ -0,0 +1,6 @@
+#create a tuple  
+a=()
+for i in range(1,10):
+    n=input('Enter any string: ')
+    a += (n,)
+print(a)    
