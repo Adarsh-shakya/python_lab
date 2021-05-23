07f6817f782733ba7f1db3c148f47e4dbd83d120
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-05-23 15:23:13 +0530

    Update sum_of_matrix.py

--- sum_of_matrix.py
+++ sum_of_matrix.py
@@ -1,26 +1,20 @@
-r=int(input('Enter the row of matrix: '))
-c=int(input('Enter the column of matrix: '))
-matrixA=[]
-for i in range(r):
-    a=[]
-    for j in  range(c):
-        k=int(input("Enter the element of matrix A: "))
-        a.append(k)
-    matrixA.append(a)    
-matrixB=[]
-for i in range(r):
-    a=[]
-    for j in  range(c):
-        k=int(input("Enter the element of matrix B : "))
-        a.append(k)
-    matrixB.append(a)
-matrixC=[]    
-for i in range(r):
-    a=[]
-    for j in range(c):
-        a.append(matrixA[i][j]+matrixB[i][j])
-    matrixC.append(a)
-for i in range(r):
-    for j in range(c):
-        print(matrixC[i][j],end=' ')
-    print()    
+#sum of two matrix 
+l=int(input('Enter the number of row: '))
+m=[]
+n=[]
+o=[]
+for x in range(2):
+    print('Enter the element for matrix')
+    for i in range(l):
+        k=list(map(int, input('Enter the row of matrix: ').split()))
+        if x==0:
+          m.append(k)
+        else:
+          n.append(k)
+for j in range(l):
+    s=[]
+    for i in range(len(m[0])):
+        s.append(m[j][i]+n[j][i])
+    o.append(s)
+for h in o:
+    print(*h)
